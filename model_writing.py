54fe9086d8b91c27a192078d1e946d6c4b621afc
Gastón <gasty.cavallo@gmail.com>
2021-06-17 16:34:48 -0300

    Update model_writing.py

--- model_writing.py
+++ model_writing.py
@@ -9,25 +9,52 @@ Created on Tue Jun 15 11:38:49 2021
 import tensorflow as tf
 
 #Define a function to create models
-def create_model():
+def create_model1():
 
-    cnn_model = tf.keras.models.Sequential()
+    cnn_model1 = tf.keras.models.Sequential()
 
-    cnn_model.add(tf.keras.layers.Conv1D(8, 4, padding = 'valid', activation = 'relu', strides = 1, input_shape = (512, 4)))
-    cnn_model.add(tf.keras.layers.Conv1D(4, 16, padding = 'valid', activation = 'relu', strides = 1))
-    cnn_model.add(tf.keras.layers.Flatten())
-    cnn_model.add(tf.keras.layers.Dense(2, activation='softmax'))
+    cnn_model1.add(tf.keras.layers.Conv1D(8, 4, padding = 'valid', activation = 'relu', strides = 1, input_shape = (512, 4)))
+    cnn_model1.add(tf.keras.layers.Conv1D(4, 16, padding = 'valid', activation = 'relu', strides = 1))
+    cnn_model1.add(tf.keras.layers.Flatten())
+    cnn_model1.add(tf.keras.layers.Dense(2, activation='softmax'))
 
-    cnn_model.summary()
+    cnn_model1.summary()
 
-    return cnn_model
+    return cnn_model1
+
+def create_model2():
+
+    cnn_model2 = tf.keras.models.Sequential()
+
+    cnn_model2.add(tf.keras.layers.Conv1D(8, 4, padding = 'valid', activation = 'relu', strides = 1, input_shape = (512, 4)))
+    cnn_model2.add(tf.keras.layers.Conv1D(4, 16, padding = 'valid', activation = 'relu', strides = 1))
+    cnn_model2.add(tf.keras.layers.Flatten())
+    cnn_model2.add(tf.keras.layers.Dense(3, activation='softmax'))
+    cnn_model2.add(tf.keras.layers.Dense(2, activation='softmax'))
+
+    cnn_model2.summary()
+
+    return cnn_model2
+
+def create_model3():
+
+    cnn_model3 = tf.keras.models.Sequential()
+
+    cnn_model3.add(tf.keras.layers.Conv1D(10, 4, padding = 'valid', activation = 'relu', strides = 1, input_shape = (512, 4)))
+    cnn_model3.add(tf.keras.layers.Conv1D(50, 13, padding = 'valid', activation = 'relu', strides = 13))
+    cnn_model3.add(tf.keras.layers.Flatten())
+    cnn_model3.add(tf.keras.layers.Dense(100, activation='softmax'))
+    cnn_model3.add(tf.keras.layers.Dense(2, activation='softmax'))
+
+    cnn_model3.summary()
+
+    return cnn_model3
+
+#puedo hacer un for que recorra todos los modelos definidos, los ejecute y los guarde
 #Create a basic model instance
-model_appsim1 = create_model()
-
+model_appsim3 = create_model3()
 #Display model architecture
-model_appsim1.summary()
-
+model_appsim3.summary()
 #Save the model architecture
-
-model_appsim1.save('/Users/gastoncavallo/Desktop/Facultad/PI/models_arch/model_appsim1')
+model_appsim3.save('/Users/gastoncavallo/Desktop/Facultad/PI/models_arch/model_appsim1')
 
